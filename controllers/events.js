f11d29216c89b70d451af1af95befb8ddc0c8de6
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-12-26 15:21:51 -0300

    Events crud finished

--- controllers/events.js
+++ controllers/events.js
@@ -2,11 +2,12 @@ const { response } = require('express');
 const Event = require('../models/Event');
 
 
-const getEvents = (req, res = response) => {
+const getEvents = async (req, res = response) => {
+    const events = await Event.find().populate('user', 'name');
     res.json({
         ok: true,
-        msg: 'getEvents'
-    })
+        events
+    });
 }
 
 
@@ -29,19 +30,83 @@ const addEvent = async (req, res = response) => {
 }
 
 
-const updateEvent = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    })
+const updateEvent = async (req, res = response) => {
+
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const event = await Event.findById( eventId );
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe evento con ese ID'
+            });
+        }
+        
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene permisos para editar este evento'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+
+        res.json({
+            ok: true,
+            evento: updatedEvent
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Dirigase al administrador'
+        });
+    }
+
+    
 }
 
 
-const deleteEvent = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    })
+const deleteEvent = async (req, res = response) => {
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const event = await Event.findById( eventId );
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe evento con ese ID'
+            });
+        }
+        
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene permisos para eliminar este evento'
+            });
+        }
+
+        await Event.findByIdAndDelete( eventId );
+
+        res.json({
+            ok: true,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Dirigase al administrador'
+        });
+    }
 }
 
 
